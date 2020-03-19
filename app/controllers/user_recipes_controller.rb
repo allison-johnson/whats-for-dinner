4e930483e706b53305fe3c58f5f0b52f7fd66af7
allison_johnson <allison.cuttler@gmail.com>
2020-03-18 21:41:45 -0700

    user can view their saved recipes (recipe box)

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -7,7 +7,6 @@ class UserRecipesController < ApplicationController
     end
     
     def create
-      binding.pry 
       UserRecipe.create(user_id: params[:user_id], recipe_id: params[:recipe_id])
       redirect_to user_recipes_path
     end #create
